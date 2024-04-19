32a853de44e8a2a22b179511b9bede87fec602bc
sadhna-madaan <142089721+sadhna-madaan@users.noreply.github.com>
2024-04-19 20:25:30 +0530

    Add files via upload

---
+++ calculator/assests/calculator.js
@@ -0,0 +1,14 @@
+// function fun(){
+//     let output=document.getElementById('button').value;
+//     console.log(output);
+//     document.getElementById('input').value.innerHTML=output;
+
+   
+// }
+
+// function result(){
+//     // let result=document.getElementById('input').value;
+//     let output=eval(document.getElementById('input').value);
+//     console.log(output);
+//     document.getElementById('input').value.innerHTML=output;
+// }
